83c6b89f746a602ed22c07512c17c46ea2512c8e
Keti <k.palavandishvili@yahoo.com>
2021-04-12 19:11:34 +0200

    first function is working

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,9 +1,37 @@
 // Write your code here
-let images = ["images/kitten1.jpg", "images/kittens2.jpg"]
+let images = ["images/kitten1.jpg", "images/kittens2.jpg", "images/kitty3.png", "images/kitty4.jpg", "images/kitty5.jpg"]
 let img = document.querySelector(".slideShow img")
+let i = 0;
 
-images.src ="images/kitten1.jpg"
-img.style.border = "10px solid"
+uploadImages();
+function uploadImages() {
+  img.src = images[i];
+  
+}
+
+function moveBack() {
+  if (i === 0) {
+    i = images.length - 1 ;
+   
+  } else {
+     i = i-1; 
+  }
+  uploadImages();
+}
+
+
+function moveForward() {
+  
+}
+
+// let buttonAutoBack = document.getElementById("auto-back").addEventListener("click", moveAutoBack);
+let buttonBack = document.getElementById("back").addEventListener("click", moveBack);
+// let buttonStop = document.getElementById("stop").addEventListener("click", stopIt);
+// let buttonAutoForward = document.getElementById("auto-forward").addEventListener("click", moveAutoForward);
+let buttonForward = document.getElementById("forward").addEventListener("click", moveForward);
+
+// console.log(buttonAutoBack);
+
 
 
 
